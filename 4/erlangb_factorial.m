52ac9d43bbfd630bf27894c95f742bcdf30347b7
nikpag00 <nikospagonas00@gmail.com>
2021-06-01 09:56:59 +0300

    Finish 4th exercise

---
+++ 4/erlangb_factorial.m
@@ -0,0 +1,8 @@
+function B_result = erlangb_factorial(rho, c)
+  summation = 0;
+  for k = 0:c
+    summation = summation + rho^k/factorial(k);
+  endfor
+  
+  B_result = rho^c/factorial(c)/summation;
+endfunction
